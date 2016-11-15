471a4e521d3003189c3c54e3d2a9a2e619634e66
Anastasia Gorbikova <a.tayanovskaya@gmail.com>
2016-11-15 17:01:14 +0300

    Added RWD to the project

--- ES6/app.js
+++ ES6/app.js
@@ -5,22 +5,28 @@
 	const SOURCE_BY_DEFAULT = 'bbc-news';
 	const API_KEY = '2fabd738608345058dedf508d3c9b9ab';
 
-
-	var newsListElement = document.querySelector('.news-list'),
-		sourceListElement = document.querySelector('.source-list'),
-		source = SOURCE_BY_DEFAULT,
-		articlesUrl = updateArticleUrl(),
-		articleTmplContent = document.querySelector('#SingleArticle').content;
-			
-
-	function updateArticleUrl(){
+	const updateArticleUrl = () => {
 		return `https:\/\/newsapi.org\/v1\/articles?source=${source}&apiKey=${API_KEY}`;
 	}
 
-	function onSourcesLoaded(sources) {
-		var documentFragment = document.createDocumentFragment();
 
-		for (let source of sources) {
+	let newsListElement = document.querySelector('.news-list'),
+		sourceListElement = document.querySelector('.source-list'),
+		source = SOURCE_BY_DEFAULT,
+		articlesUrl = updateArticleUrl(),
+		articleTmplContent = document.querySelector('#SingleArticle').content,
+		navButtonElement = document.querySelector('.nav-button');
+	
+
+	navButtonElement.addEventListener('click', (event) => {
+		event.preventDefault();
+		sourceListElement.classList.toggle('visible');
+	})	
+
+	function onSourcesLoaded(sources) {
+		let documentFragment = document.createDocumentFragment();
+
+		sources.forEach(source => {
 			let sourceObj = new Source(source);
 			sourceObj.createDOMElement();
 			documentFragment.appendChild(sourceObj.element);
@@ -28,11 +34,11 @@
 			if (source.id === SOURCE_BY_DEFAULT) {
 				sourceObj.element.classList.add('active');
 			}
-		}
+		})
 
 		sourceListElement.appendChild(documentFragment);
-		sourceListElement.addEventListener('click', event => {
-			var target = event.target;
+		sourceListElement.addEventListener('click', function(event) {
+			let target = event.target;
 			if (target.tagName.toLowerCase() === 'li') {
 				let activeListElement = sourceListElement.querySelector('li.active');
 				activeListElement.classList.remove('active');
@@ -41,20 +47,22 @@
 				source = target.dataset.sourceId;
 				articlesUrl = updateArticleUrl();
 				Requester.getNewsList(articlesUrl, onNewsListLoaded, onNewsListError);
+
+				this.classList.remove('visible');
 			}
 		})
 	}
 
 	function onNewsListLoaded(articles) {
-		var	documentFragment = document.createDocumentFragment();
+		let	documentFragment = document.createDocumentFragment();
 
 		newsListElement.innerHTML = '';
 
-		for (let article of articles) {
+		articles.forEach(article => {
 			let newsArticle = new Article(article);
 			let articleWithData = newsArticle.insertDataIntoTemplate(articleTmplContent);
 			documentFragment.appendChild(articleWithData);
-		}
+		})
 
 		newsListElement.appendChild(documentFragment);
 	}
