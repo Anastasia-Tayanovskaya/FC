89a6bd90d4568c200f699b28bbac312781380668
Anastasia Gorbikova <a.tayanovskaya@gmail.com>
2016-12-19 10:29:18 +0300

    removed comments

--- Nodejs/workshop/routes/index.js
+++ Nodejs/workshop/routes/index.js
@@ -7,19 +7,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// router.get('/articles', function(req, res, next) {
-//   Article.find({}, (err, data) => {
-//     console.log(data);
-//     res.render('article', { articleData: data  });
-//   })
-// });
-
-// router.get('/add-new', function(req, res, next) {
-//   res.render('add-new', {});
-// });
-
-// router.post('/', function(req,res){
-// });
-
-
 module.exports = router;
