16508ff35017378d6429ee4e55463ddbb0739100
Anastasia Gorbikova <a.tayanovskaya@gmail.com>
2017-01-06 12:48:03 +0300

    REST API

--- Nodejs/workshop/routes/articles.js
+++ Nodejs/workshop/routes/articles.js
@@ -14,7 +14,7 @@ let storage = multer.diskStorage({
 });
 let upload = multer({ storage: storage });
 
-var isAuthenticated = function (req, res, next) {
+let isAuthenticated = function (req, res, next) {
 	if (req.isAuthenticated()) {
 		return next();
   }
@@ -23,9 +23,11 @@ var isAuthenticated = function (req, res, next) {
 
 
 router.get('/', isAuthenticated, function(req, res, next) {
-  Article.find({}, (err, data) => {
-    console.log(data);
-    res.render('article', { articleData: data  });
+  Article.find({}, (err, articles) => {
+    console.log(articles);
+    if (err) { res.send(err); }
+    res.json(articles);
+    //res.render('article', { articleData: articles });
   })
 });
 
@@ -33,66 +35,111 @@ router.get('/add-new', function(req, res, next) {
   res.render('add-new', {});
 });
 
-router.post('/add', upload.single('urlToImage'), function(req,res){
+router.post('/', upload.single('urlToImage'), function(req, res){
     console.log(req.body);
-    let body = req.body;
+    let body = req.body,
+        image = (req && req.file && req.file.filename) ? req.file.filename : 'no_image.png';
     let article = new Article({
         title: body.title, 
         description: body.description,
         url: body.url,
-        urlToImage: 'uploads/' + req.file.filename,
+        urlToImage: 'uploads/' + image,
         author: body.author
     });
     article.save(function (err, article) {
-        if (err) return console.error(err);
-        res.redirect('/articles');
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'Article created!' });
+        //res.redirect('/api/articles');
     });
 });
 
-router.post('/delete', function(req, res){
-    let articleId = req.body.base_id;
-    Article.remove({_id: articleId}, function(err) {
-        res.redirect('/articles');
+router.get('/:article_id', isAuthenticated, function(req, res) {
+    Article.findById(req.params.article_id, function(err, article) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(article);
     });
-})
-
-router.get('/update/:id', function(req, res){
-    Article.find({_id: req.params.id}, (err, data) => {
-       res.render('update-article', {articleData: data[0], id: req.params.id});
-    })
 });
 
+// router.post('/delete', function(req, res){
+//     let articleId = req.body.base_id;
+//     Article.remove({_id: articleId}, function(err) {
+//         res.redirect('/articles');
+//     });
+// })
 
-router.post('/update', upload.single('urlToImage'), function(req,res){
-    console.log(req.body);
+// router.get('/update/:id', function(req, res){
+//     Article.find({_id: req.params.id}, (err, data) => {
+//        res.render('update-article', {articleData: data[0], id: req.params.id});
+//     })
+// });
+
+router.delete('/:article_id', function(req, res) {
+        Article.remove({
+            _id: req.params.article_id
+        }, function(err, article) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+router.put('/:article_id', upload.single('urlToImage'), function(req, res) {
     let body = req.body,
-        articleId = body.articleId,
-        query = { _id: articleId },
-        updatedObj = {}, urlToImage;
-    
-    if (req.file && req.file.filename) {
-        urlToImage = 'uploads/' + req.file.filename;
-        updatedObj = {
+        image = (req && req.file && req.file.filename) ? req.file.filename : 'no_image.png',
+        updatedArticle = new Article({
             title: body.title, 
             description: body.description,
             url: body.url,
-            urlToImage: urlToImage,
+            urlToImage: 'uploads/' + image,
             author: body.author
-        }
-    }
-    else {
-        updatedObj = {
-            title: body.title, 
-            description: body.description,
-            url: body.url,
-            author: body.author
-        }
-    }
+        });
 
-    Article.update(query, updatedObj, {}, function (err, article) {
-        if (err) return console.error(err);
-        res.redirect('/articles');
+    Article.update({ _id: req.params.article_id }, updatedArticle, {}, function (err, article) {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'Article updated!' });
     })
 });
+
+
+
+// router.post('/update', upload.single('urlToImage'), function(req,res){
+//     console.log(req.body);
+//     let body = req.body,
+//         articleId = body.articleId,
+//         query = { _id: articleId },
+//         updatedObj = {}, urlToImage;
+    
+//     if (req.file && req.file.filename) {
+//         urlToImage = 'uploads/' + req.file.filename;
+//         updatedObj = {
+//             title: body.title, 
+//             description: body.description,
+//             url: body.url,
+//             urlToImage: urlToImage,
+//             author: body.author
+//         }
+//     }
+//     else {
+//         updatedObj = {
+//             title: body.title, 
+//             description: body.description,
+//             url: body.url,
+//             author: body.author
+//         }
+//     }
+
+//     Article.update(query, updatedObj, {}, function (err, article) {
+//         if (err) return console.error(err);
+//         res.redirect('/articles');
+//     })
+// });
 
 module.exports = router;
